6a1f39290dac6683ca510d839c1f2dbcab94dfec
arvind1983 <41881552+arvind1983@users.noreply.github.com>
2019-12-12 11:42:07 +0800

    Update ModusEcho.js

--- www/ModusEcho.js
+++ www/ModusEcho.js
@@ -4,6 +4,10 @@ exports.echo = function(arg0, success, error) {
     exec(success, error, "ModusEcho", "echo", [arg0]);
 };
 
+exports.getCookies = function(success, error) {
+    exec(success, error, "ModusEcho", "getCookies");
+};
+
 exports.echojs = function(arg0, success, error) {
     if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
         success(arg0);
